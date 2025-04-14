bfb0ecc8446fb777a9131260de636bdc4efaaf5f
Ilya Peganov <ilia.peganov5965@gmail.com>
2025-04-15 01:22:40 +0300

    adapt solver.py to the new version

--- app/solver.py
+++ app/solver.py
@@ -1,4 +1,5 @@
-from app.game_config import Field, PlayerType, Cell
+from app.field import Field
+from app.player import Player
 from app.node import Node
 from enum import Enum
 import sys
@@ -19,7 +20,7 @@ DIRECTIONS = [
 ]
 
 
-analyzed_positions: dict[int, tuple[PositionStatus, Cell]] = {}
+analyzed_positions: dict[int, tuple[PositionStatus, Field.Cell]] = {}
 
 
 def check_win(current_state: Node) -> bool:
@@ -54,7 +55,7 @@ def check_win(current_state: Node) -> bool:
     return False
 
 
-def get_position_status_and_best_move(current_state: Node) -> tuple[PositionStatus, Cell]:
+def get_position_status_and_best_move(current_state: Node) -> tuple[PositionStatus, Field.Cell]:
     """
     Возращает оценку позиции и оптимальный ход
     """
@@ -64,35 +65,35 @@ def get_position_status_and_best_move(current_state: Node) -> tuple[PositionStat
         return analyzed_positions[current_position_hash]
     
     if check_win(current_state):
-        analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Cell())
+        analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Field.Cell())
         return analyzed_positions[current_position_hash]
     
     if not current_state.free_cells_count:
-        analyzed_positions[current_position_hash] = (PositionStatus.DRAW_POSITION, Cell())
+        analyzed_positions[current_position_hash] = (PositionStatus.DRAW_POSITION, Field.Cell())
         return analyzed_positions[current_position_hash]
     
-    analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Cell())
+    analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Field.Cell())
     changed: bool = False
-    current_player: PlayerType = current_state.who_moves
-    current_last_move: Cell = current_state.last_move
+    current_player: Player.Type = current_state.who_moves
+    current_last_move: Field.Cell = current_state.last_move
 
-    current_state.who_moves = PlayerType(abs(current_state.who_moves.value - 1))
+    current_state.who_moves = Player.Type(abs(current_state.who_moves.value - 1))
     current_state.free_cells_count -= 1
     
     for row in range(Field.HEIGHT):
         for col in range(Field.WIDTH):
-            if current_state.field[row][col] != PlayerType.NONE:
+            if current_state.field[row][col] != Player.Type.NONE:
                 continue
 
             current_state.field[row][col] = current_player
-            current_state.last_move = Cell(row, col)
+            current_state.last_move = Field.Cell(row, col)
 
             next_position_status: PositionStatus = get_position_status_and_best_move(current_state)[0]
 
-            current_state.field[row][col] = PlayerType.NONE
+            current_state.field[row][col] = Player.Type.NONE
 
             if next_position_status == PositionStatus.LOSING_POSITION:
-                analyzed_positions[current_position_hash] = (PositionStatus.WINNING_POSITION, Cell(row, col))
+                analyzed_positions[current_position_hash] = (PositionStatus.WINNING_POSITION, Field.Cell(row, col))
 
                 current_state.who_moves = current_player
                 current_state.free_cells_count += 1
@@ -101,11 +102,11 @@ def get_position_status_and_best_move(current_state: Node) -> tuple[PositionStat
                 return analyzed_positions[current_position_hash]
             elif next_position_status == PositionStatus.DRAW_POSITION:
                 if analyzed_positions[current_position_hash][0] == PositionStatus.LOSING_POSITION:
-                    analyzed_positions[current_position_hash] = (PositionStatus.DRAW_POSITION, Cell(row, col))
+                    analyzed_positions[current_position_hash] = (PositionStatus.DRAW_POSITION, Field.Cell(row, col))
                     changed = True
             else:
                 if analyzed_positions[current_position_hash][0] == PositionStatus.LOSING_POSITION and not changed:
-                    analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Cell(row, col))
+                    analyzed_positions[current_position_hash] = (PositionStatus.LOSING_POSITION, Field.Cell(row, col))
                     changed = True
 
     current_state.who_moves = current_player
