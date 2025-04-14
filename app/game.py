22cd58c2167925bad5959401805fb7b508d4201a
Ilya Peganov <ilia.peganov5965@gmail.com>
2025-04-15 02:37:25 +0300

    removed code duplication of functions check_win

--- app/game.py
+++ app/game.py
@@ -2,6 +2,7 @@ from app.game_config import PROGRAM_VERSION, PROGRAM_VERSION_DESCRIPTION
 from app.field import Field
 from app.player import Player
 from app.node import Node
+from app.solver import check_win
 
 import copy
 from typing import ForwardRef
@@ -230,7 +231,7 @@ Your command is >   """
         Проверяет состояние игры
         """
 
-        if self.__check_win():
+        if check_win(self.current_state):
             return (
                 Game.GameStates.CROSS_WON
                 if self.current_state.who_moves == Player.Type.NAUGHT
@@ -241,47 +242,6 @@ Your command is >   """
             return Game.GameStates.TIE
 
         return Game.GameStates.CONTINUE
-
-    def __check_win(self) -> bool:
-        """
-        Проверяет, закончилась ли игра победой
-        """
-
-        DIRECTIONS = [
-            (-1, 0),
-            (-1, 1),
-            (0, 1),
-            (1, 1),
-            (1, 0),
-            (1, -1),
-            (0, -1),
-            (-1, -1),
-        ]
-
-        for direction in range(4):
-            count = 1
-
-            for _ in range(2):
-                row = self.current_state.last_move.row + DIRECTIONS[direction][0]
-                col = self.current_state.last_move.col + DIRECTIONS[direction][1]
-
-                while (
-                    0 <= row < Field.HEIGHT
-                    and 0 <= col < Field.WIDTH
-                    and self.current_state.field[row][col]
-                    == self.current_state.field[self.current_state.last_move.row][
-                        self.current_state.last_move.col
-                    ]
-                ):
-                    count += 1
-                    row += DIRECTIONS[direction][0]
-                    col += DIRECTIONS[direction][1]
-
-                direction = (direction + 4) % 8
-            if count >= Field.STREAK_TO_WIN:
-                return True
-
-        return False
 
     def __reset_game(self) -> None:
         """
