12de01a5e9dde023460284e7495f3ea7ba5d0f25
Dmitry Sorochan <pinta@Dmitry-Laptop-2.local>
2025-06-01 17:07:05 +0300

    no important changes

--- app/game_config.py
+++ app/game_config.py
@@ -1,8 +1,7 @@
 PROGRAM_VERSION = 0
-PROGRAM_VERSION_DESCRIPTION = "console version. m, n, k -- arbitrary"
+PROGRAM_VERSION_DESCRIPTION = "pygame version. m, n, k -- arbitrary, d -- #TODO"
 
 
-# game parameters  #TODO спрашивать при запуске?
 CONST_FIELD_HEIGHT = 3
 CONST_FIELD_WIDTH = 3
 CONST_STREAK_TO_WIN_SIZE = 3
